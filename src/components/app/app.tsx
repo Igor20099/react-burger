6422662a139a6d20a42b1fc5b5495e4916c248c4
Igor <megakill123@yandex.ru>
2023-03-10 21:11:17 +0500

    add app.jsx

---
+++ src/components/app/app.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './app.module.css';
+import AppHeader from '../app-header/app-header';
+import BurgerIngredients from '../burger-ingredients/burger-ingredients';
+import BurgerConstructor from '../burger-constructor/burger-constructor';
+import {ingredients} from '../../utils/ingredients';
+
+function App() {
+  const URL = 'https://norma.nomoreparties.space/api/ingredients'
+  const [data,setData] = React.useState([])
+  React.useEffect(() => {
+      fetch(URL).then(res => res.json()).then(res => setData(res.data)).catch((err) => {
+        console.log(err);
+      })
+  }, [])
+ 
+  return (
+    <div className={styles.app} >
+      <AppHeader />
+      <main className={styles.content}>
+        <BurgerIngredients data={data}/>
+        <BurgerConstructor ingredients={ingredients}/>
+      </main>
+      
+    </div>
+  );
+}
+
+export default App;
