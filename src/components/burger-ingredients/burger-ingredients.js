ab7717e8065a8298bd61bc76f0fe3aaf35fc8da1
Igor <megakill123@yandex.ru>
2023-04-03 00:25:22 +0500

    add switch tab at scroll in burger-ingredients.js

--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -9,40 +9,70 @@ import PropTypes from "prop-types";
 import styles from "./burger-ingredients.module.css";
 import IngredientDetails from "../ingredient-details/ingredient-details";
 import Modal from "../modal/modal";
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 import { getIngredients } from "../../services/actions/ingredients";
-import { getIngredient, deleteIngredient } from "../../services/actions/ingredient-details";
+import {
+  getIngredient,
+  deleteIngredient,
+} from "../../services/actions/ingredient-details";
 
 function BurgerIngredients() {
-  const {ingredients} = useSelector(state => state.ingredients)
+  const { ingredients } = useSelector((state) => state.ingredients);
   const [current, setCurrent] = React.useState("one");
-  const {ingredient} = useSelector(state=> state.ingredientDetails)
+  const { ingredient } = useSelector((state) => state.ingredientDetails);
 
-
-  const ingredientsContainerRef = useRef(null)
-  console.log(ingredientsContainerRef.current)
-  const bunRef = useRef(null)
-  console.log(bunRef.current)
-  const mainRef = useRef(null)
-  console.log(mainRef.current.getBoundingClientRect().top)
+  const ingredientsContainerRef = useRef(null);
+  const bunRef = useRef(null);
+  const mainRef = useRef(null);
+  const sauseRef = useRef(null);
 
   const dispatch = useDispatch();
-    
-  React.useEffect(()=> {
-      dispatch(getIngredients())
-      
-  }, [dispatch])
+
+  const handleScroll = () => {
+    if (
+      bunRef.current.getBoundingClientRect().top ===
+        ingredientsContainerRef.current.getBoundingClientRect().top &&
+      bunRef.current.getBoundingClientRect().bottom >
+        ingredientsContainerRef.current.getBoundingClientRect().top
+    ) {
+      setCurrent("one");
+    } else if (
+      sauseRef.current.getBoundingClientRect().top <=
+        ingredientsContainerRef.current.getBoundingClientRect().top &&
+      sauseRef.current.getBoundingClientRect().bottom >
+        ingredientsContainerRef.current.getBoundingClientRect().top
+    ) {
+      setCurrent("two");
+    } else if (
+      mainRef.current.getBoundingClientRect().top <=
+        ingredientsContainerRef.current.getBoundingClientRect().top &&
+      mainRef.current.getBoundingClientRect().bottom >
+        ingredientsContainerRef.current.getBoundingClientRect().top
+    ) {
+      setCurrent("three");
+    }
+  };
+
+  React.useEffect(() => {
+    dispatch(getIngredients());
+    document
+      .querySelector("#ingredients-container")
+      .addEventListener("scroll", handleScroll);
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const handleOpenModal = (e) => {
     ingredients.forEach((el) => {
       if (e.currentTarget.id === el._id) {
-        dispatch(getIngredient(el))
+        dispatch(getIngredient(el));
       }
     });
   };
 
   const handleCloseModal = () => {
-    dispatch(deleteIngredient())
+    dispatch(deleteIngredient());
   };
 
   const modal = (
@@ -66,7 +96,11 @@ function BurgerIngredients() {
           Начинки
         </Tab>
       </div>
-      <div className={styles.ingredients_container} ref={ingredientsContainerRef}>
+      <div
+        id="ingredients-container"
+        className={styles.ingredients_container}
+        ref={ingredientsContainerRef}
+      >
         <div className="bun-container" ref={bunRef}>
           <h2 className="text text_type_main-medium">Булки</h2>
           <ul className={styles.bun_conteiner}>
@@ -99,7 +133,7 @@ function BurgerIngredients() {
             })}
           </ul>
         </div>
-        <div className="sauce-container" ref={mainRef}>
+        <div className="sauce-container" ref={sauseRef}>
           <h2 className="text text_type_main-medium">Соусы</h2>
           <ul className={styles.bun_conteiner}>
             {ingredients.map((el) => {
@@ -126,7 +160,7 @@ function BurgerIngredients() {
             })}
           </ul>
         </div>
-        <div className="main-container">
+        <div className="main-container" ref={mainRef}>
           <h2 className="text text_type_main-medium">Начинки</h2>
           <ul className={styles.bun_conteiner}>
             {ingredients.map((el) => {
@@ -159,21 +193,22 @@ function BurgerIngredients() {
 }
 
 BurgerIngredients.propTypes = {
-  data: PropTypes.arrayOf( PropTypes.shape({
-    _id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    type: PropTypes.string.isRequired,
-    proteins: PropTypes.number.isRequired,
-    fat: PropTypes.number.isRequired,
-    carbohydrates: PropTypes.number.isRequired,
-    calories: PropTypes.number.isRequired,
-    price: PropTypes.number.isRequired,
-    image: PropTypes.string.isRequired,
-    image_mobile: PropTypes.string.isRequired,
-    image_large: PropTypes.string.isRequired,
-    __v: PropTypes.number.isRequired,
-  }).isRequired
-  )
-}
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      proteins: PropTypes.number.isRequired,
+      fat: PropTypes.number.isRequired,
+      carbohydrates: PropTypes.number.isRequired,
+      calories: PropTypes.number.isRequired,
+      price: PropTypes.number.isRequired,
+      image: PropTypes.string.isRequired,
+      image_mobile: PropTypes.string.isRequired,
+      image_large: PropTypes.string.isRequired,
+      __v: PropTypes.number.isRequired,
+    }).isRequired
+  ),
+};
 
 export default BurgerIngredients;
