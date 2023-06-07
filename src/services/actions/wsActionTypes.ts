081ea13cee417cf2fca7da450700dbeae503f4f0
Igor <megakill123@yandex.ru>
2023-06-07 19:33:13 +0500

    edit socketMiddleware from javascript in typescript code

--- src/services/actions/wsActionTypes.ts
+++ src/services/actions/wsActionTypes.ts
@@ -4,3 +4,33 @@ export const WS_CONNECTION_ERROR:'WS_CONNECTION_ERROR' = 'WS_CONNECTION_ERROR';
 export const WS_CONNECTION_CLOSED:'WS_CONNECTION_CLOSED' = 'WS_CONNECTION_CLOSED';
 export const WS_GET_ORDERS:'WS_GET_ORDERS' = 'WS_GET_ORDERS';
 export const WS_SEND_ORDER:'WS_SEND_ORDER' = 'WS_SEND_ORDER';
+
+export interface IWsConnectionStartAction {
+    readonly type: typeof WS_CONNECTION_START;
+  }
+
+export interface IWsConnectionSuccessAction {
+    readonly type: typeof WS_CONNECTION_SUCCESS;
+  }
+
+export interface IWsConnectionErrorAction {
+    readonly type: typeof WS_CONNECTION_ERROR;
+  } 
+
+export interface IWsConnectionClosedAction {
+    readonly type: typeof WS_CONNECTION_CLOSED;
+  }   
+
+export interface IWsGetOrdersAction {
+    readonly type: typeof WS_GET_ORDERS;
+  }  
+
+export interface IWsSendOrderAction {
+    readonly type: typeof WS_SEND_ORDER;
+  }  
+  
+export type TWsActions = | IWsConnectionStartAction | IWsConnectionSuccessAction | IWsConnectionErrorAction | IWsConnectionClosedAction | IWsGetOrdersAction | IWsSendOrderAction
+  
+export type TWsActionNames = {
+    [key in TWsActions['type']] : key
+}
--- src/services/middleware/socketMiddleware.ts
+++ src/services/middleware/socketMiddleware.ts
@@ -3,12 +3,13 @@ import { Middleware, MiddlewareAPI } from "redux";
 import { WS_URL } from "../../utils/constants";
 import { getCookie } from "../../utils/cookie";
 import { tokenRequest } from "../actions/auth";
+import { TWsActionNames, TWsActions } from "../actions/wsActionTypes";
 
-export const socketMiddleware = (wsActionTypes) => {
-  return (store) => {
-    let socket = null;
+export const socketMiddleware = (wsActionTypes:TWsActionNames) => {
+  return (store:any) => {
+    let socket:any = null;
 
-    return (next) => (action) => {
+    return (next:any) => (action:any) => {
       const { dispatch, getState } = store;
       const { type, payload } = action;
 
@@ -17,31 +18,31 @@ export const socketMiddleware = (wsActionTypes) => {
       }
 
       if (socket) {
-        socket.onopen = (event) => {
+        socket.onopen = (event :WebSocketEventMap) => {
           dispatch({
             type: wsActionTypes.WS_CONNECTION_SUCCESS,
             payload: event,
           });
         };
 
-        socket.onerror = (event) => {
+        socket.onerror = (event:WebSocketEventMap) => {
           dispatch({ type: wsActionTypes.WS_CONNECTION_ERROR, payload: event });
         };
 
-        socket.onmessage = (event) => {
+        socket.onmessage = (event:WebSocketEventMap & {data:string}) => {
           const { data } = event;
           const newData = JSON.parse(data)
           dispatch({ type: wsActionTypes.WS_GET_ORDERS, payload: newData });
         };
 
-        socket.onclose = (event) => {
+        socket.onclose = (event:WebSocketEventMap) => {
           dispatch({
             type: wsActionTypes.WS_CONNECTION_CLOSED,
             payload: event,
           });
         };
 
-        if (type === wsActionTypes.WS_SEND_MESSAGE) {
+        if (type === wsActionTypes.WS_SEND_ORDER) {
           const order = payload;
           socket.send(JSON.stringify(order));
         }
