f8f4642f52ea25e3929f5f666420a37af0b1de60
Igor <megakill123@yandex.ru>
2023-06-07 16:55:31 +0500

    edit modal-overlay and cookie in typescript code

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -1,14 +1,14 @@
-export function getCookie(name) {
+export function getCookie(name:string) {
     const matches = document.cookie.match(
         new RegExp('(?:^|; )' + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + '=([^;]*)')
     );
     return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
-export function setCookie(name, value, props) {
-    props = props || {};
+
+export function setCookie(name: string, value: string, props?: any): void {
     let exp = props.expires;
-    if (typeof exp == 'number' && exp) {
+    if (typeof exp === 'number' && exp) {
         const d = new Date();
         d.setTime(d.getTime() + exp * 1000);
         exp = props.expires = d;
@@ -19,15 +19,17 @@ export function setCookie(name, value, props) {
     value = encodeURIComponent(value);
     let updatedCookie = name + '=' + value;
     for (const propName in props) {
-        updatedCookie += '; ' + propName;
-        const propValue = props[propName];
-        if (propValue !== true) {
-            updatedCookie += '=' + propValue;
+        if (Object.prototype.hasOwnProperty.call(props, propName)) {
+            updatedCookie += '; ' + propName;
+            const propValue = props[propName];
+            if (propValue !== true) {
+                updatedCookie += '=' + propValue;
+            }
         }
     }
     document.cookie = updatedCookie;
-}
+} 
 
-export function delCookie(name) {
+export function delCookie(name:string) {
     setCookie(name, '', { expires: -1 });
 }
